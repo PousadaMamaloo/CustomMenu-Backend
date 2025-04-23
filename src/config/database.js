a6c73519ba07932ae6b481ca5ddc8073c4e832d5
Ian Jairo <41593870+IanJairo@users.noreply.github.com>
2025-04-23 15:47:02 -0300

    Update: Arquivo de configuração do Database

--- src/config/database.js
+++ src/config/database.js
@@ -3,22 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('🔍 Variáveis de conexão:');
-console.log('DB:', process.env.POSTGRES_DB);
-console.log('HOST:', process.env.POSTGRES_HOST);
-console.log('PORT:', process.env.POSTGRES_PORT);
-
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
-    port: Number(process.env.POSTGRES_PORT),
-    logging: console.log,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: console.log,
+});
 
 try {
   await sequelize.authenticate();
