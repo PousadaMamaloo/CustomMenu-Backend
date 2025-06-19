e53f1bc0a53b9c3be2315d2d8b8b2aef9d88e7f6
João Gabriel S. Dantas <gabrielsd.10000@gmail.com>
2025-06-19 14:07:07 -0300

    fix: aumento do limite do tamanho da requisicao

--- src/index.js
+++ src/index.js
@@ -26,8 +26,8 @@ app.use(cors({
 
 app.use(cookieParser());
 
-app.use(express.json({ limit: '2mb' }));
-app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 app.get("/", (req, res) => {
   res.send("Testando o get princiapl com mudança!");
