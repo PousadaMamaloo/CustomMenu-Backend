1da717bade3f8b10d23c5e8ae57226bbdbd41760
João Gabriel S. Dantas <gabrielsd.10000@gmail.com>
2025-07-09 21:36:06 -0300

    Fix/ erro ao puxar categorias

--- src/rotas/itemRotas.js
+++ src/rotas/itemRotas.js
@@ -8,12 +8,21 @@ const router = express.Router();
 
 router.use(autenticador);
 
+<<<<<<< Updated upstream
 router.post('/criar', autorizaAdministrador, itemValidador, criarItem);
 router.put('/atualizar/:id', autorizaAdministrador, itemValidador, atualizarItem);
 router.delete('/excluir/:id', autorizaAdministrador, excluirItem);
 router.get('/listar/:id', buscarItem)
 router.get('/listar', listarItens);
 router.get('/categorias', listarCategoriasUnicas);
+=======
+router.post('/', autorizaAdministrador, itemValidador, criarItem);
+router.get('/categorias', listarCategoriasUnicas);
+router.put('/:id', autorizaAdministrador, itemValidador, atualizarItem);
+router.delete('/:id', autorizaAdministrador, excluirItem);
+router.get('/', listarItens);
+router.get('/:id', buscarItem)
+>>>>>>> Stashed changes
 
 export default router;
 
